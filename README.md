3f0ff8cbb9a825d1dd078f302c28691a2c31537f
Briefly <brieflyx@gmail.com>
2015-04-27 13:52:03 +0800

    dot product

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Some picked-up code from my codewars practice, including many skills.
 Updating slowly ...
 
-Visit [codewars](http://www.codewars.com/) for more.
+Visit [codewars](http://www.codewars.com/r/Wkv56w) for more.
---
+++ min_dot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+__author__ == 'Pei Zhongyu'
+
+# use map & reduce for min dot product for 2 vectors
+def min_dot(a, b):
+    return reduce(lambda x,y: x+y, map(lambda x,y:x*y, sorted(a), sorted(b, reverse=True)))
+
+if __name__ == '__main__':
+    print min_dot([1,2,3,4,5,6], [0,1,1,1,0,0])
+    
